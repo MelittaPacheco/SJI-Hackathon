d56dac1f80d2d469e063350ac71747d94c0f4f5b
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2023-02-10 19:11:54 +0530

    Added Cors

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const express = require("express") ;
-const connectDB = require("./config/db.js") ;
-const routes = require("./routes/index.js") ; 
+const express = require("express");
+const connectDB = require("./config/db.js");
+const routes = require("./routes/index.js");
 require("./config/db");
 const app = express();
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // JSON
 app.use(express.json());
